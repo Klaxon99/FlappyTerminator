000d6b10d1bd799768341ed67d911d41e51bb0f2
Klaxon99 <maxloda2013@yandex.ru>
2024-02-19 18:07:17 +0500

    delete trash from Ground component

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Ground : MonoBehaviour, IInteractable
-{
-}
+public class Ground : MonoBehaviour, IInteractable {}
